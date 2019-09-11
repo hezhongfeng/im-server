ebbeb22461fac66ebc883535a22d8cb8f03df612
hezhongfeng <287645587@qq.com>
2019-09-11 19:07:25 +0800

    添加database相关

--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,11 @@
 'use strict';
 
-/** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+exports.io = {
+  enable: true,
+  package: 'egg-socket.io',
+};
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
 };
