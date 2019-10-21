a6e1f02168be472436d85428552ee38608ed6caf
hezhongfeng <287645587@qq.com>
2019-10-21 18:08:25 +0800

    fix(authorize): 修复注册用户名重复

--- app/controller/v1/login.js
+++ app/controller/v1/login.js
@@ -2,10 +2,9 @@ const Controller = require('egg').Controller;
 
 class LoginController extends Controller {
   async signup() {
-    const { ctx, service } = this;
-    const req = Object.assign(ctx.request.body);
+    const { service } = this;
     // 调用 Service 进行业务处理
-    service.login.create(req);
+    await service.login.create();
   }
 }
 
--- app/service/login.js
+++ app/service/login.js
@@ -4,8 +4,23 @@ class LoginService extends Service {
   async create() {
     const ctx = this.ctx;
     const { username, password } = ctx.request.body;
-    const auth = await ctx.model.Authorization.create({ provider: 'local', username, password });
-    ctx.body = auth;
+    const auth = await ctx.model.Authorization.findOne({ where: { provider: 'local', username } });
+    if (auth) {
+      ctx.body = {
+        code: '101',
+        message: '用户名重复',
+      };
+      return;
+    }
+
+    try {
+      const auth = await ctx.model.Authorization.create({ provider: 'local', username, password });
+      ctx.body = {
+        id: auth.id,
+      };
+    } catch (error) {
+      console.warn('error', error.errors[0].validatorKey);
+    }
   }
 }
 
