ea8d854c52addc4b85139dce63f5c1ea29287a87
hezhongfeng <287645587@qq.com>
2020-04-02 11:13:12 +0800

    feat(upload): 优化多文件上传

--- app/controller/v1/upload.js
+++ app/controller/v1/upload.js
@@ -6,20 +6,25 @@ class uploadController extends Controller {
   async upload() {
     const { ctx } = this;
 
-    const file = ctx.request.files[0];
-    // const filePath = `upload/${Math.random().toString()}` + path.basename(file.filename);
-    const filePath = `upload/${Math.random().toString()}` + path.basename('123.mp4');
-
-    // const file = ctx.request.body.files.file; // 获取上传文件
-    const reader = fs.createReadStream(file.path); // 创建可读流
-    // const ext = file.name.split('.').pop(); // 获取上传文件扩展名
-    const upStream = fs.createWriteStream(filePath); // 创建可写流
-    reader.pipe(upStream);
+    const data = [];
+    for (const file of ctx.request.files) {
+      // 生成文件路径，注意upload文件路径需要存在
+      const filePath = `./upload/${Date.now() +
+        Math.floor(Math.random() * 100000).toString() +
+        '.' +
+        file.filename.split('.').pop()}`;
+      const reader = fs.createReadStream(file.filepath); // 创建可读流
+      const upStream = fs.createWriteStream(filePath); // 创建可写流
+      reader.pipe(upStream); // 可读流通过管道写入可写流
+      data.push({
+        url: filePath
+      });
+    }
 
     ctx.body = {
       statusCode: '0',
       errorMessage: null,
-      data: [{ url: filePath }]
+      data: data
     };
   }
 }
