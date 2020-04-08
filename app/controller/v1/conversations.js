89867605244d3a585ab8131f147248e17641dd43
hezhongfeng <287645587@qq.com>
2020-04-08 14:34:49 +0800

    feat(会话): 添加会话列表查询

--- app/controller/v1/conversations.js
+++ app/controller/v1/conversations.js
@@ -4,15 +4,45 @@ class ConversationController extends Controller {
   // INDEX
   async index() {
     const { ctx } = this;
+    // const Op = Sequelize.Op;
+    const { Op } = this.app.Sequelize;
 
     // 注意这里需要经过两次查询
     const user = await ctx.model.User.findByPk(ctx.session.user.id);
-    const Conversations = await user.getConversations();
+    const conversations = await user.getConversations();
+    let data = [];
+    for (const iterator of conversations) {
+      let conversation = {
+        id: iterator.id,
+        type: iterator.type,
+        createdAt: iterator.createdAt,
+        updatedAt: iterator.updatedAt,
+      };
+      conversation.targetId = '1212234';
+      if (conversation.type === 'chat') {
+        let users = await iterator.getUsers({
+          where: {
+            id: {
+              [Op.ne]: ctx.session.user.id,
+            },
+          },
+        });
+        conversation.targetId = users.pop().id;
+      } else if (conversation.type === 'groupchat') {
+        let group = await ctx.model.Group.findOne({
+          where: {
+            conversationId: iterator.id,
+          },
+        });
+        conversation.targetId = group.id;
+      }
+      data.push(conversation);
+    }
 
     ctx.body = {
       statusCode: '0',
       errorMessage: null,
-      data: Conversations
+      data: data,
     };
   }
 }
